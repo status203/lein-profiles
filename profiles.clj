fe553393c281a745979841f2c7fa85cd89f1ba91
status203 <git@status203.com>
2021-12-22 16:23:28 +0000

    Added lein-ancient

--- profiles.clj
+++ profiles.clj
@@ -1,5 +1,6 @@
 {:user {:plugins [[lein-try "0.4.3"]
-                  [com.jakemccrary/lein-test-refresh "0.24.1"]]
+                  [com.jakemccrary/lein-test-refresh "0.24.1"]
+                  [lein-ancient "1.0.0-RC3"]]
         :dependencies [[pjstadig/humane-test-output "0.11.0"]]
         :injections [(require 'pjstadig.humane-test-output)
                      (pjstadig.humane-test-output/activate!)]
